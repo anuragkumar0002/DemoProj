c1994bca233627025ce4c71422c8af431f251e7c
Anurag Kumar <41521155+anuragkumar0002@users.noreply.github.com>
2025-07-21 17:27:47 +0530

    Update CameraStreamingApplication.java

--- src/main/java/com/streaming/CameraStreamingApplication.java
+++ src/main/java/com/streaming/CameraStreamingApplication.java
@@ -3,14 +3,17 @@ package com.streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class CameraStreamingApplication extends SpringBootServletInitializer {
 
- @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(CameraStreamingApplication.class);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CameraStreamingApplication.class);
+	}
 
 
 	public static void main(String[] args) {
